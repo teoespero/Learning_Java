72eb064806533fb8c0be749cfce638697a5c67ee
Teo Espero <38012835+teoespero@users.noreply.github.com>
2022-07-29 15:08:21 -0700

    Add files via upload

---
+++ Chap03_Exercises/ComputeAreaV2.java
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  ComputeArea.java
+//  Author: Teo Espero
+//          BS Cloud and System Administration
+//          BS Software Development
+//          Western Governors University
+//  Description: This program in Java was written to demo the computation of a Circle's area.
+//  Date: 07282022
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// define the packages we need
+import javax.swing.plaf.TableHeaderUI;
+import java.util.Scanner;
+
+// class declaration
+public class ComputeAreaV2 {
+
+    // main function
+    public static void main(String[] args) {
+
+        // define our vars
+        double area = 0.00;
+
+        // create the Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // prompt the user for the value for radius
+        System.out.print("> Enter the value for radius: ");
+        double radius = input.nextDouble();
+
+        if (radius < 0)
+            System.out.println("The radius can't be less than 0.");
+        else {
+            // perform the computations
+            area = (radius * radius) * 3.14159;
+
+            // display the result
+            System.out.println("> The area for the circle of radius " + radius + " is " + area);
+        }
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
